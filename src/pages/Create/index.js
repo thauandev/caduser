be115fb6bbd4f8bafac4c89341d7ad9d3e79c4b7
thauandev <thauandepaula@icloud.com>
2020-01-14 11:09:55 -0300

    Implementando redux

---
+++ src/pages/Create/index.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { connect, useDispatch } from 'react-redux';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
+
+import { Container } from './styles';
+
+const schema = Yup.object().shape({
+  id: Yup.number().required('O id é obrigatório'),
+  name: Yup.string().required('O nome é obrigatório'),
+  email: Yup.string()
+    .email('Insira um email válido')
+    .required('O e-mail é obrigatório'),
+  street: Yup.string(),
+  suite: Yup.string(),
+  city: Yup.string(),
+  zipcode: Yup.string(),
+  phone: Yup.number(),
+});
+
+function Create() {
+  const dispatch = useDispatch();
+
+  const handleSubmit = user => {
+    dispatch({
+      type: 'ADD_TO_CART',
+      user,
+    });
+  };
+
+  return (
+    <Container>
+      <h2>Formulário</h2>
+      <Form className="formulario" schema={schema} onSubmit={handleSubmit}>
+        <Input name="id" placeholder="ID" size="40" />
+        <Input name="name" placeholder="Name" size="40" />
+        <Input name="email" placeholder="Email" size="40" />
+
+        <Input name="street" placeholder="Street" size="40" />
+        <Input name="suite" placeholder="Suite" size="40" />
+        <Input name="city" placeholder="City" size="40" />
+        <Input name="zipcode" placeholder="Zip-Code" size="40" />
+
+        <Input name="phone" placeholder="Phone" size="40" />
+        <button type="submit">Validar</button>
+      </Form>
+    </Container>
+  );
+}
+
+export default connect()(Create);
--- src/store/modules/list/reducer.js
+++ src/store/modules/list/reducer.js
@@ -1,3 +1,9 @@
-export default function list() {
-  return [];
+export default function list(state = [], action) {
+  console.log(state);
+  switch (action.type) {
+    case 'ADD_TO_LIST':
+      return [...state, action.user];
+    default:
+      return state;
+  }
 }
